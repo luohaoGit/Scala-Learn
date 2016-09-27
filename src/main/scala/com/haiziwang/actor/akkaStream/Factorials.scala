4fdb15b8b0460dd2835bf89816b9f48833834592
luohao <553569682@qq.com>
2016-09-27 17:55:45 +0800

    source sink runnable

---
+++ src/main/scala/com/haiziwang/actor/akkaStream/Factorials.scala
@@ -0,0 +1,38 @@
+package com.haiziwang.actor.akkaStream
+
+import java.nio.file.Paths
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream._
+import akka.stream.scaladsl._
+import akka.util.ByteString
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.concurrent.{Await, Future}
+
+/**
+  * Created by luohao on 2016/9/27.
+  */
+object Factorials extends App {
+
+  implicit val system = ActorSystem("QuickStart")
+  implicit val materializer = ActorMaterializer()
+
+  val source: Source[Int, NotUsed] = Source(1 to 100)
+  source.runForeach(println)
+
+  val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
+
+  def lineSink(fileName: String): Sink[String, Future[IOResult]] = {
+    Flow[String]
+      .map(num => ByteString(s"$num\n"))
+      .toMat(FileIO.toPath(Paths.get(fileName)))(Keep.right)
+  }
+
+  Await.ready(factorials.map(_.toString)
+    .runWith(lineSink("temp/factorials2.txt")), 3 second)
+
+  system.terminate()
+}
